3ae74bdb0552cfb431e90597159513664a313882
tapsikk <tapsgame@gmail.com>
2024-05-26 21:16:15 +0300

    gC - r.

---
+++ src/scripts/generateContacts.js
@@ -0,0 +1,18 @@
+import fs from 'node:fs/promises';
+import { PATH_DB } from '../constants/contacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+
+const generateContacts = async (number) => {
+  try {
+  const contacts = await fs.readFile(PATH_DB, "utf-8");
+  const parseContacts = JSON.parse(contacts);
+  for (let i = 0; i < number; i += 1){
+    parseContacts.push(createFakeContact());
+    }
+    await fs.writeFile(PATH_DB, JSON.stringify(parseContacts, null, 2))
+  } catch (error){
+  console.log(error);
+  }
+};
+
+await generateContacts(5);
